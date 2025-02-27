30f96858673268a85eab325faa02286c1ee3fc37
topshager <rem28777@gmail.com>
2025-02-27 22:11:23 +0200

    Update iloc.py

--- iloc.py
+++ iloc.py
@@ -4,3 +4,6 @@ Auto_re.iloc [[3 ,4]]
 Auto_re.iloc [: ,[0 ,2 ,3]]
 Auto_re.iloc [[3 ,4] ,[0 ,2 ,3]]
 Auto_re.loc['ford galaxie 500 ', ['mpg ', 'origin ']]
+
+idx_80 = Auto_re['year'] > 80
+Auto_re.loc[idx_80,['weight','origin']]
