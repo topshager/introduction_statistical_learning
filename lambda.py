ff9344ef791ac3918db11dca637da1ac79cc603c
topshager <rem28777@gmail.com>
2025-02-27 22:16:15 +0200

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,2 +1,8 @@
 Auto_re.loc[lambda df:df['year'] > 80 ,['weight','origin']]
 Auto_re.loc[lambda df:(df['year'] > 80) & (df['mpg']>30),['weight','origin']]
+
+Auto_re.loc[lambda df: (df['displacement '] < 300)
+& (df.index.str.contains ('ford ')
+| df.index.str.contains ('datsun ')),
+['weight ', 'origin ']
+]
