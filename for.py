f99b3d4aa42e74819c3c9e2e3e3953a730c0cdad
topshager <rem28777@gmail.com>
2025-02-27 22:22:40 +0200

    Create for.py

---
+++ for.py
@@ -0,0 +1,11 @@
+total = 0
+for value in [3,2,19]:
+    total += value
+print('Total is: {0}'.format(total))
+
+
+total = 0
+for value in [2,3,44]:
+    for weight in [4,5,2]:
+        total+= value + weight
+print('total is:{0}'.format(total))
