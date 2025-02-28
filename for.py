938abf5ec8f858550630b00469c82b1adefb0c4e
topshager <rem28777@gmail.com>
2025-02-28 09:13:35 +0200

    Update for.py

--- for.py
+++ for.py
@@ -9,3 +9,9 @@ for value in [2,3,44]:
     for weight in [4,5,2]:
         total+= value + weight
 print('total is:{0}'.format(total))
+
+
+total = 0
+for value, weight in zip([2,3,19],[0.2,0.3,0.5]):
+        total += weight * value
+print('Weighted average is: {0} '.format(total))
