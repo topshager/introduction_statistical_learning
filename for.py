6b146ae3d696e0dd17a9dcfeca2d7d300ca25e84
topshager <rem28777@gmail.com>
2025-02-28 09:24:53 +0200

    Update for.py

--- for.py
+++ for.py
@@ -15,3 +15,8 @@ total = 0
 for value, weight in zip([2,3,19],[0.2,0.3,0.5]):
         total += weight * value
 print('Weighted average is: {0} '.format(total))
+
+
+for col in D.columns:
+    template = 'column "{0}" has {1:.2%} missing values'
+    print(template.format(col,np.isnan(D[col]).mean()),)
